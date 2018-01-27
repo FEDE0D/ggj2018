225c6f58f836fa2e60b064e3017f4ad34d2c420a
Fran <Delfrank94@gmail.com>
2018-01-26 21:59:49 -0300

    movimiento super basico

--- GGJ/Player.gd
+++ GGJ/Player.gd
@@ -4,7 +4,27 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 
+var speed = Vector2(0,0)
+var move_speed = 300
+var direction = Vector2(0,0)
+
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	set_process(true)
 	pass
+	
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	else:
+		direction.y = 0
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+	elif Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	else:
+		direction.x = 0
+	
+	speed = direction * move_speed * delta
+	set_pos(get_pos() + speed)
