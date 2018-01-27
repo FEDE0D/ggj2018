7d43f6f736c7a2abdf5a786040493160e5431ceb
Manuel <manuel.markethero@gmail.com>
2018-01-27 19:26:05 -0300

    Add shadow to score

--- GGJ/progressBar.gd
+++ GGJ/progressBar.gd
@@ -6,11 +6,13 @@ func update(count, score):
 	get_node("Control/TextureProgress").set_value(count)
 	get_node("Control/TextureProgress/AnimationPlayer").play("glow")
 	get_node("Control/Score").set_text(str(score))
+	get_node("Control/ScoreShadow").set_text(str(score))
 	print(score)
 
 
 func _ready():
 	get_node("Control/Score").set_text(str(0))
+	get_node("Control/ScoreShadow").set_text(str(0))
 	set_process(true)
 
 func _process(delta):
