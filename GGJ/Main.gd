1688ce156cde1df1c990c86815b1b254469d17dd
Federico Pacheco <federicogpacheco@gmail.com>
2018-01-28 15:55:24 -0300

    updates

--- GGJ/Main.gd
+++ GGJ/Main.gd
@@ -5,3 +5,6 @@ func _ready():
 	for spawner in get_node("map/spawners").get_children():
 		spawner.spawn(get_node("characters"))
 	Globals.get("contador").update()
+
+func end_game():
+	SceneTransition.transition_to("scenes/ui/main.tscn")
